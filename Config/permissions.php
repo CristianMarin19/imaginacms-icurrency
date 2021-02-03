862fe3602b1c84d0a446e2ee37a8db95b559b038
Juan Escalante <jcec007@gmail.com>
2021-02-02 19:57:01 -0500

    +Add: Manage Permissions

--- Config/permissions.php
+++ Config/permissions.php
@@ -2,6 +2,7 @@
 
 return [
     'icurrency.currencies' => [
+        'manage' => 'icurrency::currencies.manage resource',
         'index' => 'icurrency::currencies.list resource',
         'create' => 'icurrency::currencies.create resource',
         'edit' => 'icurrency::currencies.edit resource',
